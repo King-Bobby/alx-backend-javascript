62c7aaf9045a0244af42b2d2948e3a31025f9b49
King-Bobby <noelbrowndboss@gmail.com>
2023-10-06 22:48:09 +0000

    ..

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+function getStudentsByLocation(students, city) {
+  // Use the filter function to get students located in the specified city
+  const studentsInCity = students.filter((student) => student.location === city);
+  return studentsInCity;
+}
+
+export default getStudentsByLocation;
