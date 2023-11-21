ebece75f0b211a3909a4ec271ab87c6176f8ce7e
King-Bobby <noelbrowndboss@gmail.com>
2023-11-21 20:56:58 +0000

    1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+// 1-stdin.js
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+
+  if (name !== '') {
+    process.stdout.write(`Your name is: ${name}\n`);
+  } else {
+    process.exit();
+  }
+});
+
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
