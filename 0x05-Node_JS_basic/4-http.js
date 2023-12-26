0f9f9311bd38df6d9a0d794ccbcf917c0a943ae3
King-Bobby <noelbrowndboss@gmail.com>
2023-12-26 23:35:18 +0000

    4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,19 @@
-// 4-http.js
-
 const http = require('http');
 
 // Create an HTTP server
 const app = http.createServer((req, res) => {
-  // Set the response header
+  // Set the response headers
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-  // Send the response
+  // Respond with the message
   res.end('Hello Holberton School!\n');
 });
 
 // Listen on port 1245
-app.listen(1245);
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
 
+// Export the app variable
 module.exports = app;
