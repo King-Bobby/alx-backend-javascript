80a575a12c996c63355b696ac38d3e29dd09577f
King-Bobby <noelbrowndboss@gmail.com>
2023-12-27 17:53:10 +0000

    mocha

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,13 +9,15 @@ describe('calculateNumber', () => {
 
     it('should return the sum of rounded numbers for one decimal place', () => {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
+	assert.strictEqual(calculateNumber(1.6, 2), 4);
     });
-    it('should return the sum when the second number is already round', () => {
-	assert.strictEqual(calculateNumber(1.3, 2), 3);
+    it('should return the sum when using math.ceil with math.round', () => {
+	assert.strictEqual(calculateNumber(1.6, 2), 4);
+	assert.strictEqual(calculateNumber(2, 1.7), 4);
     });
 
     it('should return the sum of rounded numbers for two decimal places', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.6, 3.7), 6);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
