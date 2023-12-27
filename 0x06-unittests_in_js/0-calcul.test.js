fb52b5ef7cca7ae65a796ad353fbce6a728c86e6
King-Bobby <noelbrowndboss@gmail.com>
2023-12-27 17:37:30 +0000

    mocha test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,19 @@
+// 0-calcul.test.js
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should round the numbers and return their sum', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+    it('should return the sum of rounded numbers for integers', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return the sum of rounded numbers for one decimal place', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+	assert.strictEqual(calculateNumber(1.3, 2), 3);
+    });
+
+    it('should return the sum of rounded numbers for two decimal places', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
 });
