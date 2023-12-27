9207fa5b8c644c2fe1bbe6e1621485becfcd8266
King-Bobby <noelbrowndboss@gmail.com>
2023-12-27 17:56:18 +0000

    ..

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,7 +9,7 @@ describe('calculateNumber', () => {
 
     it('should return the sum of rounded numbers for one decimal place', () => {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
-	assert.strictEqual(calculateNumber(1.6, 2), 4);
+	assert.strictEqual(calculateNumber(1.3, 2), 3);
     });
     it('should return the sum when using math.ceil with math.round', () => {
 	assert.strictEqual(calculateNumber(1.6, 2), 4);
@@ -18,6 +18,6 @@ describe('calculateNumber', () => {
 
     it('should return the sum of rounded numbers for two decimal places', () => {
         assert.strictEqual(calculateNumber(1.6, 3.7), 6);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
 });
